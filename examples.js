bf8e27822ba78582297ee93a40e3e91b7bdeb1e0
Lauren Beatty <lauren.m.beatty@gmail.com>
2015-09-18 13:13:40 -0500

    initial commit

--- examples.js
+++ examples.js
@@ -1,13 +1,54 @@
 
+// $(document).ready(function() {
+
+//     $.ajax('data.json', {
+//         success: function(response){
+//             console.log(response);
+//         },
+//         error: function(request, errorType, errorMessage){
+//             alert(errorType + " " + errorMessage);
+//         }
+//     });
+
+// })
+
 $(document).ready(function() {
+	$.ajax({
+		url: "data.json"
+	}).done(function(response){
+		console.log(response);
+		for (var i = 0; i<response.length; i++){
+			console.log(response[i]);
+			var $content = $("<div class = 'group' style='color:" + response[i].value + "'>" + "The Color # " + (i+1) + " is:" + "</div>");
+			$(".content-placeholder").append($content);
 
-    $.ajax('data.json', {
-        success: function(response){
-            console.log(response);
-        },
-        error: function(request, errorType, errorMessage){
-            alert(errorType + " " + errorMessage);
-        }
-    });
+			var $colorType = $("<p style = 'color:" + response[i].value + "'>" + response[i].color + " known as hex #: " + response[i].value + "</p>");
+			console.log($colorType);
 
-})
+			$(".group").parent().append($colorType);
+
+			//console.log(response[i].value);
+
+			//$("#color-" + i).parent().css("color", response[i].value)
+
+//$("#color-" + (i+1).parent().css("color", response[i].value);
+
+		}
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
